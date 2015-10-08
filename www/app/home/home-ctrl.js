5a8afbe65cfb9a7f5adf16b86a2baa59dab1ed20
skwill <stephanwilliams1@gmail.com>
2015-10-07 21:44:09 -0400

    made updates to the pages backgrounds, added functionality to change the homepage based on time

--- www/app/home/home-ctrl.js
+++ www/app/home/home-ctrl.js
@@ -27,7 +27,29 @@ angular.module('metApp').run(function($rootScope) {
 			vm.title = data.query.results.channel.item.title;
 			vm.sunrise = data.query.results.channel.astronomy.sunrise;
 			vm.sunset = data.query.results.channel.astronomy.sunset;
-			console.log(data);
+
+			var d = new Date();
+    		var n = d.getHours();
+
+    		var demo = document.getElementById("demo");
+
+    		if (n >= 6 && n < 12) {
+    			demo.style.background =  "url('https://hereandthereblogger.files.wordpress.com/2011/12/iphone-4-background-28.jpg') no-repeat center center";
+    		}else if (n >= 12 && n < 13) {
+    			demo.style.background =  "url('http://www.ilikewallpaper.net/iphone-5-wallpapers/download/11473/Miniature-City-2-iphone-5-wallpaper-ilikewallpaper_com.jpg') no-repeat center center"; 	
+    		}else if (n >= 15 && n < 18) {  
+    			demo.style.background =  "url('http://www.ilikewallpaper.net/iphone-4s-wallpapers/download/8047/Apple-Black-Background-iphone-4s-wallpaper-ilikewallpaper_com.jpg') no-repeat center center"; 		
+    		}else{
+    			demo.style.background =  "url('http://icetothebrim.com/wp-content/uploads/2012/01/iphone_space_ampersand_blur.png')  no-repeat center center";
+    			
+    		};
+
+    		console.log(data);
+
+    		
+
+    		
+			//console.log(n);
 			/*vm.meta = data._meta;
 			vm.links = data._links;*/
 			//vm.bulletins = data.items[0];
